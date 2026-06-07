25d81a425ce6335d7ec9e5d7d4003ddb76c10d09
Gaurav41507Gaurav41507git config --global user.email <gauravsarkar2017@gmail.com>
2026-06-08 02:25:52 +0530

    fix: add suspense boundary for URL filters

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
 import ProductGrid from "@/components/ProductGrid";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main className="bg-[#FAF9F6]">
         <div className="mx-auto flex flex-col gap-[3rem] px-[4%] py-[1.5rem] lg:flex-row">
-          <Sidebar />
+
+          <Suspense fallback={<div>Loading Filters...</div>}>
+            <Sidebar />
+          </Suspense>
 
           <ProductGrid />
         </div>
